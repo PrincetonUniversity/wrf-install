c0dea23fa99c8ba2c19e51892d5db80de3d1d394
MichaelIgbinoba123 <77029334+MichaelIgbinoba123@users.noreply.github.com>
2024-01-21 19:48:45 -0500

    Update intel-mpi-env.sh

--- intel-mpi-env.sh
+++ intel-mpi-env.sh
@@ -1,13 +1,12 @@
-#! /bin/bash
+#!bin/sh
+
 module purge
 
 module load intel/19.1
-module load intel-mpi/intel/2019.7/64
+module load intel-mpi/intel/2019.7
 module load hdf5/intel-17.0/intel-mpi/1.10.0
 module load netcdf/intel-16.0/hdf5-1.8.16/intel-mpi/4.4.0
 
-export NETCDF=/usr/local/netcdf/intel-16.0/hdf5-1.8.16/intel-mpi/4.4.0
-export HDF5=/usr/local/hdf5/intel-17.0/intel-mpi/1.10.0
-export JASPERLIB=/projects/GEOCLIM/wrf/wrf_libs_intel/lib
-export JASPERINC=/projects/GEOCLIM/wrf/wrf_libs_intel/include
+export NETCDF=/usr/local/share/Modules/modulefiles/netcdf/intel-16.0/hdf5-1.8.16/intel-mpi/4.4.0
+export HDF5=/usr/local/share/Modules/modulefiles/hdf5/intel-17.0/intel-mpi/1.10.0
 export NETCDF_classic=1
